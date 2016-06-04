32c3e6a3389f0a8bd4b757eb83fb98a77c7ca819
Otto <ottorottier@gmail.com>
2016-06-04 22:55:34 +0200

    Sqlite backend and user model to keep track of the tokens

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,17 +13,25 @@
 
 use Illuminate\Http\Request;
 use App\Jobs\SendSlackMessage;
-use Illuminate\Support\Facades\Crypt;
+use App\User;
 
 $app->get('/', function () use ($app) {
     return view('spa');
+});
+
+$app->get('/api/users', function () use ($app) {
+    return User::all();
 });
 
 $app->get('/api/status', function (Request $request) use ($app) {
     $auth = $request->header('X-Authorization');
     $loggedIn = false;
     if ($auth && strpos($auth, 'Bearer ') === 0) {
-        $loggedIn = Crypt::decrypt(substr($auth, 7));
+        $token = substr($auth, 7);
+        $user = User::where('token', $token)->first();
+        if ($user) {
+            $loggedIn = $user->username;
+        }
     }
     return [
         'status' => 'amazeballs',
@@ -33,7 +41,14 @@ $app->get('/api/status', function (Request $request) use ($app) {
 
 $app->post('/api/request-token', function (Request $request) use ($app) {
     $username = $request->input('username');
-    $token = Crypt::encrypt($username);
+    $token = substr(md5(rand()), 0, 12);
+
+    $user = User::firstOrNew([
+        'username' => $username,
+    ]);
+    $user->token = $token;
+    $user->save();
+
     $success = dispatch(new SendSlackMessage('@' . $username, 'here is your token: `' . $token . '`'));
     return [
         'username' => $username,
--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'username'
     ];
 
     /**
@@ -27,6 +27,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password',
+        'token',
     ];
 }
