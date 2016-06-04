e644700f80df6fa9968d122b2d3a020077b6ef79
Otto <ottorottier@gmail.com>
2016-06-04 19:56:23 +0200

    Single Page Application with Vue

---
+++ public/script.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#app',
+    data: {
+        header: 'Welcome',
+        message: 'to the kantoortuin'
+    }
+})
---
+++ resources/views/spa.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>De Kantoortuin</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+
+<div id='app'>
+    <h1>{{ header }}</h1>
+    <p>{{ message }}</p>
+</div>
+
+<script src="/vue.js"></script>
+<script src="/script.js"></script>
+</body>
+
+</html>
