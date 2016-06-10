77a642574b0cac84d00555bb70e8d51054e8ae8d
Otto <ottorottier@gmail.com>
2016-06-10 17:18:59 +0200

    Optimize composer install

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,10 @@ chmod 600 ssh_deploy_key
 ssh-add ssh_deploy_key
 cp ssh_config ~/.ssh/config
 cp known_hosts ~/.ssh/known_hosts
+
+# Skip the dev dependencies
+composer install --no-interaction --prefer-dist --no-dev
 tar -zcvf release.tar.gz app artisan bootstrap database install_release.sh public resources vendor
+
 scp release.tar.gz otto@remote:/tmp
 ssh otto@remote 'mkdir -p /tmp/release && tar -xvzf /tmp/release.tar.gz -C /tmp/release > /dev/null && bash /tmp/release/install_release.sh'
