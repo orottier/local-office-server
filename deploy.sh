19acec32177618216353264fb12cf0d6426d8312
Otto <ottorottier@gmail.com>
2016-06-10 17:27:48 +0200

    Fix Travis branch check

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,20 @@
 
 set -e
 
-openssl aes-256-cbc -K $encrypted_625fb94bd2ee_key -iv $encrypted_625fb94bd2ee_iv -in secrets.tar.enc -out secrets.tar -d
-tar xvf secrets.tar
-eval "$(ssh-agent -s)"
-chmod 600 ssh_deploy_key
-ssh-add ssh_deploy_key
-cp ssh_config ~/.ssh/config
-cp known_hosts ~/.ssh/known_hosts
+if [ "$TRAVIS_BRANCH" == "master" ]; then
 
-# Skip the dev dependencies
-composer install --no-interaction --prefer-dist --no-dev
-tar -zcvf release.tar.gz app artisan bootstrap database install_release.sh public resources vendor
+    openssl aes-256-cbc -K $encrypted_625fb94bd2ee_key -iv $encrypted_625fb94bd2ee_iv -in secrets.tar.enc -out secrets.tar -d
+    tar xvf secrets.tar
+    eval "$(ssh-agent -s)"
+    chmod 600 ssh_deploy_key
+    ssh-add ssh_deploy_key
+    cp ssh_config ~/.ssh/config
+    cp known_hosts ~/.ssh/known_hosts
 
-scp release.tar.gz otto@remote:/tmp
-ssh otto@remote 'mkdir -p /tmp/release && tar -xvzf /tmp/release.tar.gz -C /tmp/release > /dev/null && bash /tmp/release/install_release.sh'
+    # Skip the dev dependencies
+    composer install --no-interaction --prefer-dist --no-dev
+    tar -zcvf release.tar.gz app artisan bootstrap database install_release.sh public resources vendor
+
+    scp release.tar.gz otto@remote:/tmp
+    ssh otto@remote 'mkdir -p /tmp/release && tar -xvzf /tmp/release.tar.gz -C /tmp/release > /dev/null && bash /tmp/release/install_release.sh'
+fi
