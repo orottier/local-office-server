d2fe43a7f785b13aab6fd0c874e8973310c4cc9e
Otto <ottorottier@gmail.com>
2016-06-12 18:46:52 +0200

    Add 'gulp watch' task to watch & compile assets
    and make jobs more granular for reusability

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,10 @@ var less = require('gulp-less');
 var path = require('path');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('compile', function() {
-    console.log('Launching rocket to compile.. 🚀');
+gulp.task('compile', ['compile-less', 'compile-js', 'compile-js-vendor']);
 
+gulp.task('compile-less', function() {
     console.log('🖇  Starting to compile LESS..');
-
     gulp.src('./resources/assets/less/main.less')
         .pipe(less({
             paths: [path.join(__dirname, 'less', 'includes')]
@@ -16,30 +15,44 @@ gulp.task('compile', function() {
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./public/assets/css'));
     console.log('✅  LESS compile complete');
+});
 
-
-
-    // JS
+gulp.task('compile-js', function() {
     console.log('🖇  Starting to compile JS..');
     gulp.src(['./resources/assets/js/components/*.js'])
         .pipe(concat('components.js'))
         .pipe(gulp.dest('./public/assets/js'));
-
-    gulp.src(['node_modules/vue/dist/vue.min.js', 'node_modules/vue-resource/dist/vue-resource.min.js', 'node_modules/vue-router/dist/vue-router.min.js'])
-        .pipe(concat('vue.js'))
-        .pipe(gulp.dest('./public/assets/js'));
     console.log('✅  JS compile complete');
 });
 
-gulp.task('lint', function lintCssTask() {
+gulp.task('compile-js-vendor', function() {
+    console.log('🖇  Bundling JS..');
+    gulp.src(['node_modules/vue/dist/vue.min.js', 'node_modules/vue-resource/dist/vue-resource.min.js', 'node_modules/vue-router/dist/vue-router.min.js'])
+        .pipe(concat('vue.js'))
+        .pipe(gulp.dest('./public/assets/js'));
+    console.log('✅  bundling JS complete');
+});
+
+gulp.task('watch', ['watch-less', 'watch-js']);
+gulp.task('watch-less', function () {
+    gulp.watch('./resources/assets/less/**/*.less', ['compile-less']);
+});
+gulp.task('watch-js', function () {
+    gulp.watch('./resources/assets/js/**/*.js', ['compile-js']);
+});
+
+gulp.task('lint', ['lint-less', 'lint-js']);
+
+gulp.task('lint-js', function () {
     console.log('🖇  Checking JS');
     var eslint = require('gulp-eslint');
     var jslint = gulp.src(['./resources/assets/js/components/*.js', './public/script.js'])
         .pipe(eslint({}))
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
+});
 
-
+gulp.task('lint-less', function () {
     console.log('🖇  Checking LESS');
     var gulpStylelint = require('gulp-stylelint');
     var stylelint = gulp.src('./resources/assets/less/*.less')
@@ -49,8 +62,4 @@ gulp.task('lint', function lintCssTask() {
                 console: true
             }]
         }));
-
-
-    var merge = require('merge-stream');
-    return merge(jslint, stylelint);
 });
